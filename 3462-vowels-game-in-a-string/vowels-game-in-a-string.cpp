bc5814d38c74bd10222996b40fbefc2a2c160291
Priyanshu tiwari <official.priyanshu.37@gmail.com>
2025-09-13 01:31:34 +0530

    Time: 4 ms (78.31%) | Memory: 18.9 MB (95.77%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        for(char c : s) {
+        for(auto& c : s) {
             if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') {
                 return true;  // Alice can always win
             }
